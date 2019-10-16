5909745fea66e9489d05e8333deeda94d529a9d0
David Rivas <david.rivas@pevaar.com>
2019-10-16 08:47:47 -0500

    Communicating with a Parent Component

--- APM-Start/src/app/shared/criteria/criteria.component.ts
+++ APM-Start/src/app/shared/criteria/criteria.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit,
+  Input, OnChanges, SimpleChanges, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'pm-criteria',
@@ -6,12 +7,22 @@ import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input, OnChang
   styleUrls: ['./criteria.component.css']
 })
 export class CriteriaComponent implements OnInit, AfterViewInit, OnChanges {
-  listFilter: string = 'cart';
   @Input() displayDetail: boolean;
   @Input() hitCount: number;
   hitMessage: string;
+  @Output() valueChange: EventEmitter<string> = new EventEmitter<string>();
 
   @ViewChild('filterElement') filterElementRef: ElementRef;
+
+  private _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.valueChange.emit(value);
+  }
 
   constructor() { }
 
